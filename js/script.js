af503a7f469313187fff2c5e469ebf79707c2968
Sudhanshu7sb <sudhanshu7.sb@gmail.com>
2020-07-04 20:29:55 +0530

    Change layout

--- js/script.js
+++ js/script.js
@@ -29,11 +29,11 @@ function showNotes() {
   let html = "";
   notesObj.forEach(function(element, index) {
     html += `
-            <div class="noteCard my-2 mx-2 card" style="width: 18rem;">
+            <div class="noteCard card" style="width: 12rem;">
                     <div class="card-body">
                         <h5 class="card-title">Note ${index + 1}</h5>
                         <p class="card-text"> ${element}</p>
-                        <button id="${index}"onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
+                        <button id="${index}"onclick="deleteNote(this.id)" class="add-note-btn">Delete Note</button>
                     </div>
                 </div>`;
   });
@@ -61,21 +61,3 @@ function deleteNote(index) {
   showNotes();
 }
 
-
-let search = document.getElementById('searchTxt');
-search.addEventListener("input", function(){
-
-    let inputVal = search.value.toLowerCase();
-    // console.log('Input event fired!', inputVal);
-    let noteCards = document.getElementsByClassName('noteCard');
-    Array.from(noteCards).forEach(function(element){
-        let cardTxt = element.getElementsByTagName("p")[0].innerText;
-        if(cardTxt.includes(inputVal)){
-            element.style.display = "block";
-        }
-        else{
-            element.style.display = "none";
-        }
-        // console.log(cardTxt);
-    })
-})
